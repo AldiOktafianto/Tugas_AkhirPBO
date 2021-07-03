7d194c0fab08beb96e55658ac8c26c1fe073c09f
Ivon Viqro Dini <80572391+ivonviqrodini@users.noreply.github.com>
2021-07-03 19:32:47 +0630

    Update MenuMahasiswaUjian.java

--- TubesPbo-PKN/src/sample/MenuMahasiswaUjian.java
+++ TubesPbo-PKN/src/sample/MenuMahasiswaUjian.java
@@ -32,11 +32,7 @@ public class MenuMahasiswaUjian {
     void back(ActionEvent event) throws IOException {
         btn_back.getScene().getWindow().hide();
         Parent root = FXMLLoader.load(getClass().getResource("../fxmlClass/MenuMahasiswa.fxml"));
-        Stage mainStage = new Stage();
-        Scene scene = new Scene(root);
-        mainStage.setScene(scene);
-        mainStage.show();
-
+       
     }
 
     @FXML
